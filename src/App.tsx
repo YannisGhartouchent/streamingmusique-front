b93c7cec24341e5fa957d0865662deddc9e594e7
YannisGhartouchent <ghartouchent@yahoo.com>
2024-07-25 15:23:05 +0200

    #6-buttons

--- src/App.tsx
+++ src/App.tsx
@@ -8,9 +8,9 @@ function App() {
     <>
       <button className="bg-black text-white font-extrabold py-2 px-10 rounded-full font-raleway">Se Connecter</button>
       <button className="bg-black  text-white font-extrabold py-2 px-10 rounded-full font-raleway">S'inscrire</button>
-      <button className="bg-stone-800 text-white font-extrabold py-2 px-10 rounded-full font-raleway">Connexion avec Google</button>
-      <button className="bg-stone-800 text-white font-extrabold py-2 px-10 rounded-full font-raleway">Connexion avec Apple</button>
-      <button className="bg-stone-800 text-white font-extrabold py-2 px-10 rounded-full font-raleway">Connexion avec Facebook</button>
+      <button className="bg-stone-700 text-white font-extrabold py-2 px-10 rounded-full font-raleway ring ring-white">Connexion avec Google</button>
+      <button className="bg-stone-700 text-white font-extrabold py-2 px-10 rounded-full font-raleway ring ring-white">Connexion avec Apple</button>
+      <button className="bg-stone-700 text-white font-extrabold py-2 px-10 rounded-full font-raleway ring ring-white">Connexion avec Facebook</button>
       <button className="bg-black text-white font-extrabold py-2 px-10 rounded-full font-raleway">Préférence</button>
       <button className="bg-red-700 text-black font-extrabold py-2 px-10 rounded-full font-raleway">PREMIUM</button>
       </>
