72afe1a66f9f5c4430efbd19faa0ce1cb7d47959
jha <j.haimez@vattenfall.fr>
2024-08-02 14:19:44 +0200

    update : fix probleme design

--- src/Application/Page/Public/Home.page.tsx
+++ src/Application/Page/Public/Home.page.tsx
@@ -1,7 +1,8 @@
 import {Logo} from "../../Component/Logo.tsx";
-import {Checkbox, Flex, Input, NavLink, Paper, Text, useMantineTheme} from "@mantine/core";
+import { Checkbox, Flex, Input, Divider, NavLink, Paper, Text, useMantineTheme } from "@mantine/core";
 import {ButtonPrimary} from "../../Component/ButtonPrimary.tsx";
 import {useState} from "react";
+import {FcGoogle} from "react-icons/fc";
 
 export const HomePage: React.FC = () => {
     const [email, setEmail] = useState("")
@@ -28,39 +29,92 @@ export const HomePage: React.FC = () => {
     
     return (
         <>
-            <Flex direction={"column"} align={"center"} justify={"center"} mt={"lg"}>
+            <Flex direction={"column"} align={"center"} justify={"center"} w={"100%"} h={"100%"}>
                 <Logo/>
-                <Paper mt={50} style={{backgroundColor: colorPaper}} pt={20} pb={20} pl={20} pr={20} w={"450px"} radius={"md"}>
-                
-                    <Input.Wrapper label={"Adresse E-mail"}>
-                    
-                        <Input
-                            value={email}
-                            placeholder={"Adresse E-mail"}
-                            onChange={(e) => setEmail(e.currentTarget.value)}
+                <Paper
+                    mt={50}
+                    style={{backgroundColor: colorPaper, display: "flex", flexDirection: "column", alignItems: "center"}}
+                    pt={20}
+                    pb={20}
+                    pl={20}
+                    pr={20}
+                    w={"450px"}
+                    radius={"md"}
+                >
+                    <Flex direction={"column"} w={"60%"} justify={"center"}>
+                        <Input.Wrapper label={"Adresse E-mail"}>
+                            <Input
+                                value={email}
+                                placeholder={"Adresse E-mail"}
+                                onChange={(e) => setEmail(e.currentTarget.value)}
+                            />
+                        </Input.Wrapper>
+                        <Input.Wrapper label={"Mot de passe"} mt="md">
+                            <Input
+                                value={password}
+                                placeholder={"Mot de passe"}
+                                onChange={(e) => setPassword(e.currentTarget.value)}
+                            />
+                        </Input.Wrapper>
+                        <Checkbox
+                            label="J'accèpte les conditions générales"
+                            mt="lg"
+                            checked={conditionChecked}
+                            onChange={() => setConditionChecked(!conditionChecked)}
                         />
-                    </Input.Wrapper>
-                    <Input.Wrapper label={"Mot de passe"} mt="md">
-                        <Input
-                            value={password}
-                            placeholder={"Mot de passe"}
-                            onChange={(e) => setPassword(e.currentTarget.value)}
-                        />
-                    </Input.Wrapper>
-                    <Checkbox
-                        label="J'accèpte les conditions générales"
-                        mt="lg"
-                        checked={conditionChecked}
-                        onChange={() => setConditionChecked(!conditionChecked)}
-                    />
-                    {conditionError && <Text>Vous devez accepter les conditions générales</Text>}
-                    <ButtonPrimary
-                    
-                     onClickButton={handlerLogin}>Se connecter</ButtonPrimary>
+                        {conditionError && <Text>Vous devez accepter les conditions générales</Text>}
+                        <ButtonPrimary
+                            onClickButton={handlerLogin}
+                            w={"50%"}
+                            mt={30}
+                            m={"auto"}
+                        >
+                            Se connecter
+                        </ButtonPrimary>
+                    </Flex>
                     <Flex w={"100%"} align={"baseline"} mt={"xs"}>
                         <Text>Tu n'as pas encore de compte APOLLON ?</Text>
-                        <NavLink c={"#ff0000"} fw={"bold"} href={"/register"} label={"Inscription"} w={"fit-content"} mt="xs"/>
+                        <NavLink
+                            bg={"transparent"}
+                            c={"#ff0000"}
+                            fw={"bold"}
+                            href={"/register"}
+                            label={"Inscription"}
+                            w={"fit-content"}
+                            mt="xs"
+                        />
                     </Flex>
+                    
+                    <Divider size={"xl"} my="md" bg={"white"} />
+                    
+                    <ButtonPrimary
+                        leftSection={<FcGoogle />}
+                        variant={"outline"}
+                        style={{border: "1px solid #fff"}}
+                        c={"white"}
+                        onClickButton={handlerLogin}
+                        w={"70%"}
+                        mt={30}
+                        m={"auto"}
+                    >
+                        Google
+                    </ButtonPrimary>
+                    <ButtonPrimary
+                        onClickButton={handlerLogin}
+                        w={"50%"}
+                        mt={30}
+                        m={"auto"}
+                    >
+                        Se connecter
+                    </ButtonPrimary>
+                    <ButtonPrimary
+                        onClickButton={handlerLogin}
+                        w={"50%"}
+                        mt={30}
+                        m={"auto"}
+                    >
+                        Se connecter
+                    </ButtonPrimary>
                 </Paper>
             </Flex>
         </>
