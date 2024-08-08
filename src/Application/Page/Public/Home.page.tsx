4c5e6a8825d1674b4fe6a5c1ede090a7ea8f79d3
YannisGhartouchent <ghartouchent@yahoo.com>
2024-08-08 15:38:54 +0200

    #16

--- src/Application/Page/Public/Home.page.tsx
+++ src/Application/Page/Public/Home.page.tsx
@@ -1,8 +1,11 @@
 import {Logo} from "../../Component/Logo.tsx";
-import { Checkbox, Flex, Input, Divider, NavLink, Paper, Text, useMantineTheme } from "@mantine/core";
+import { Checkbox, Flex, Input, Divider, NavLink, Paper, Text, useMantineTheme, PasswordInput, Switch } from "@mantine/core";
 import {ButtonPrimary} from "../../Component/ButtonPrimary.tsx";
 import {useState} from "react";
 import {FcGoogle} from "react-icons/fc";
+import { FaApple } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+
 
 export const HomePage: React.FC = () => {
     const [email, setEmail] = useState("")
@@ -30,7 +33,7 @@ export const HomePage: React.FC = () => {
     return (
         <>
             <Flex direction={"column"} align={"center"} justify={"center"} w={"100%"} h={"100%"}>
-                <Logo/>
+               <Logo/>
                 <Paper
                     mt={50}
                     style={{backgroundColor: colorPaper, display: "flex", flexDirection: "column", alignItems: "center"}}
@@ -41,23 +44,32 @@ export const HomePage: React.FC = () => {
                     w={"450px"}
                     radius={"md"}
                 >
-                    <Flex direction={"column"} w={"60%"} justify={"center"}>
+                <Flex direction={"column"} w={"60%"} justify={"center"}>
                         <Input.Wrapper label={"Adresse E-mail"}>
                             <Input
-                                value={email}
-                                placeholder={"Adresse E-mail"}
-                                onChange={(e) => setEmail(e.currentTarget.value)}
+                            value={email}
+                            placeholder={"Adresse E-mail"}
+                            onChange={(e) => setEmail(e.currentTarget.value)}
                             />
                         </Input.Wrapper>
-                        <Input.Wrapper label={"Mot de passe"} mt="md">
-                            <Input
-                                value={password}
-                                placeholder={"Mot de passe"}
-                                onChange={(e) => setPassword(e.currentTarget.value)}
+                        <Input.Wrapper  mt="md">
+                            <PasswordInput
+                            label="Mot de passe"
+                            withAsterisk
+                            value={password}
+                            placeholder={"Mot de passe"}
+                             onChange={(e) => setPassword(e.currentTarget.value)}
                             />
+                            <Switch
+                    defaultChecked
+                    color="teal"
+                    labelPosition="left"
+                    label="Se souvenir de moi"
+                    size="xs"
+                    />
                         </Input.Wrapper>
                         <Checkbox
-                            label="J'accèpte les conditions générales"
+                            label="J'accepte les conditions générales"
                             mt="lg"
                             checked={conditionChecked}
                             onChange={() => setConditionChecked(!conditionChecked)}
@@ -93,27 +105,37 @@ export const HomePage: React.FC = () => {
                         style={{border: "1px solid #fff"}}
                         c={"white"}
                         onClickButton={handlerLogin}
-                        w={"70%"}
+                        w={"50%"}
                         mt={30}
                         m={"auto"}
                     >
                         Google
                     </ButtonPrimary>
-                    <ButtonPrimary
+
+                        <ButtonPrimary
+                        leftSection={<FaApple />}
+                        variant={"outline"}
+                        style={{border: "1px solid #fff"}}
+                        c={"white"}
                         onClickButton={handlerLogin}
                         w={"50%"}
                         mt={30}
                         m={"auto"}
                     >
-                        Se connecter
+                      Apple
                     </ButtonPrimary>
+        
                     <ButtonPrimary
+                    leftSection={<FaFacebook />}
+                        variant={"outline"}
+                        style={{border: "1px solid #fff"}}
+                        c={"white"}
                         onClickButton={handlerLogin}
                         w={"50%"}
                         mt={30}
                         m={"auto"}
                     >
-                        Se connecter
+                        Facebook
                     </ButtonPrimary>
                 </Paper>
             </Flex>
