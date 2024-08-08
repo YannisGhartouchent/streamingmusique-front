14eaa261a9e7a01e1162a8d50b8c11a1c9a4da8b
jha <j.haimez@vattenfall.fr>
2024-08-08 16:20:02 +0200

    update : ajout layout private

--- src/Application/Layout/LayoutPrivate.tsx
+++ src/Application/Layout/LayoutPrivate.tsx
@@ -1,11 +1,38 @@
 import React from "react";
 import {Outlet} from "react-router-dom";
-import {Box} from "@mantine/core";
+import {AppShell} from "@mantine/core";
 
 export const LayoutPrivate: React.FC = () => {
     return (
-        <Box h={"100%"} style={{backgroundColor: "#464646"}}>
-            <Outlet/>
-        </Box>
+        <AppShell
+            h={"100%"}
+            style={{backgroundColor: "#464646"}}
+            navbar={{
+                width: 300,
+                breakpoint: 'sm',
+            }}
+            footer={{
+                height: 100,
+            }}
+        >
+            <AppShell.Navbar
+                style={{backgroundColor: "#cb0000", border: "none"}}
+            >
+                <p>je le menu lateral</p>
+                {/*  TODO : mettre les liens  */}
+            </AppShell.Navbar>
+            
+            <AppShell.Main>
+                <Outlet/>
+            </AppShell.Main>
+            
+            <AppShell.Footer
+                zIndex={999999}
+                style={{backgroundColor: "#000000", border: "none", color: "#ffffff"}}
+            >
+                <p>je suis le lecteur</p>
+            {/*  TODO : lecteur  */}
+            </AppShell.Footer>
+        </AppShell>
     )
 }
--- src/Infrastructure/Router.tsx
+++ src/Infrastructure/Router.tsx
@@ -2,8 +2,12 @@ import {createBrowserRouter} from "react-router-dom";
 import {HomePage} from "../Application/Page/Public/Home.page.tsx";
 import {LayoutPublic} from "../Application/Layout/LayoutPublic.tsx";
 import {RegisterPage} from "../Application/Page/Public/Register.page.tsx";
+import {LayoutPrivate} from "../Application/Layout/LayoutPrivate.tsx";
+import {DashboardPage} from "../Application/Page/Private/Dashboard.page.tsx";
+import {LibraryPage} from "../Application/Page/Private/Library.page.tsx";
 
 export const browserRouter = createBrowserRouter([
+    // partie public
     {
         path: "/",
         element: <LayoutPublic />,
@@ -13,8 +17,24 @@ export const browserRouter = createBrowserRouter([
                 element: <HomePage />
             },
             {
-                path: "/register",
+                path: "register",
                 element: <RegisterPage />
+            }
+        ]
+    },
+    
+    // partie private
+    {
+        path: "/dashboard",
+        element: <LayoutPrivate />,
+        children: [
+            {
+                index: true,
+                element: <DashboardPage />
+            },
+            {
+                path: "library",
+                element: <LibraryPage />
             }
         ]
     }
